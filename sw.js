7dc14dec26cd1997bc94952883abaf02570ed630
cmrnwdwrd <cmrnwdwrd@gmail.com>
2026-09-08 15:57:57 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-
-const CACHE = "bottlerag-shell-v1";
-const SHELL = ["./", "./index.html", "./manifest.webmanifest"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-  if(url.origin === self.location.origin){
-    event.respondWith(
-      caches.match(event.request).then(r => r || fetch(event.request))
-    );
+const CACHE="bottlerag-shell-v22";
+const SHELL=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===self.location.origin){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
   }
 });
